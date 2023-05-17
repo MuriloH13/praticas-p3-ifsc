f02d80d3ef7cced5f73b99aaac38861de80617c2
Murilo.H.S.Branco <murilo.b08@aluno.ifsc.edu.br>
2023-05-17 16:55:32 -0300

    Commit da Lista 05

--- lista05/src/ifsc/MainProduto.java
+++ lista05/src/ifsc/MainProduto.java
@@ -9,28 +9,31 @@ public class MainProduto {
 		Produto b = new Produto();
 		Produto c = new Produto();
 		
-		a.setNome("Tomate"); 
+		a.setNome("Tomate(Unidade)"); 
 		a.setFornecedor("Feira do Zéca");
 		a.setCodBarras((long) 812998498);
+		a.setPreco(1.0);
 		
-		b.setNome("Morango"); 
+		b.setNome("Morango(Unidade)"); 
 		b.setFornecedor("Plantação do Marcão");
 		b.setCodBarras((long) 1382278668);
+		b.setPreco(0.30);
 		
-		c.setNome("Mexirica"); 
+		c.setNome("Mexirica(Unidade)"); 
 		c.setFornecedor("Fazenda do Sérgio");
 		c.setCodBarras((long) 123454685);
+		c.setPreco(0.50);
 		
 		ArrayList<Produto> fruta = new ArrayList<Produto>();
 		fruta.add(a);
 		fruta.add(b);
 		fruta.add(c);
-		int i=fruta.size();
+		int i = fruta.size();
 		for (int n = 0; n < i; n++) {
 			System.out.println(fruta.get(n).getNome());
 			System.out.println(fruta.get(n).getFornecedor());
 			System.out.println(fruta.get(n).getCodBarras());
-			
+			System.out.println(fruta.get(n).getPreco());
 		}
 	}
 }
